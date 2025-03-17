4e02902c49010243106cbe57a8634c9159481edb
Christian Landgren <christian@landgren.nu>
2025-03-17 17:13:52 +0100

    feat: berget AI theme

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import compression from 'vite-plugin-compression';
 import type { Plugin } from 'vite';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -43,42 +44,7 @@ export default defineConfig({
         navigateFallbackDenylist: [/^\/oauth/],
       },
       includeAssets: ['**/*'],
-      manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#000000',
-        theme_color: '#009688',
-        icons: [
-          {
-            src: '/assets/favicon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/favicon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/apple-touch-icon-180x180.png',
-            sizes: '180x180',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/maskable-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
+      manifest,
     }),
     sourcemapExclude({ excludeNodeModules: true }),
     compression({
